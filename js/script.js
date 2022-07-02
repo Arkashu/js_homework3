df863d6244b73b2b8e3879e8159db501b62e49ee
Arkashu <kopeikaarkashu@gmail.com>
2022-07-02 10:20:13 +0300

    change let to const

--- js/script.js
+++ js/script.js
@@ -4,19 +4,19 @@ const a = +prompt('Enter any number:');
 const b = +prompt('Enter second number:');
 
 function sumCalc(a, b){
-    let result = a + b;
+    const result = a + b;
     console.log('Result: ' +a + ' + ' + b + ' = ' + result)
 }
 function subtractCalc(a, b){
-    let result = a - b;
+    const result = a - b;
     console.log('Result: ' +a + ' - ' + b + ' = ' + result)
 }
 function divideCalc(a, b){
-    let result = a / b;
+    const result = a / b;
     console.log('Result: ' +a + ' / ' + b + ' = ' + result)
 }
 function multiplyCalc(a, b){
-    let result = a * b;
+    const result = a * b;
     console.log('Result: ' +a + ' * ' + b + ' = ' + result)
 }
 
